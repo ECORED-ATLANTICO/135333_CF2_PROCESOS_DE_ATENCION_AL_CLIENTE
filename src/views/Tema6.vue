4257a2cb72cc08b98197a7d07cb1cc9eb6fad209
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2026-02-22 12:39:34 -0500

    tema 6

--- src/views/Tema6.vue
+++ src/views/Tema6.vue
@@ -11,82 +11,182 @@
       .col-xl-10
         p.mb-0 Los vínculos que se establecen entre las personas se llaman relaciones interpersonales, estos lazos tienen muchos rasgos como por ejemplo la convivencia y la vida como sociedad, estos vínculos se desarrollan con la familia, con los amigos, con los compañeros de trabajo y en otros escenarios de socialización, las relaciones interpersonales se inician desde la niñez y son muy importantes para desarrollar las sociedades, la construcción y el fortalecimiento de vínculos con los demás es una construcción individual.
 
-    .row.justify-center.mb-5(data-aos="fade-down") 
-      .col-12.col-lg-8
-        .bloque-texto-g.color-primario.p-3.p-sm-4.p-md-5
-          .bloque-texto-g__texto.px-5.py-4
-            p Sostener los vínculos interpersonales contribuye a la generación de habilidades individuales y sociales con la comunidad. Esto se refiere a aprender a cooperar, dialogar, expresar emociones y sentimientos, trabajar en equipo, respetar instrucciones, normas, reconocer la autoridad, cumplir con nuestras labores, negociar, empatizar y escuchar al otro.
+    .row.justify-content-center.mb-4.mb-lg-5(data-aos="fade-down") 
+      .col-12.col-xl-10
+        .bloque-texto-g.bloque-texto-g--inverso.color-primario.p-3.p-sm-4.p-md-5
           .bloque-texto-g__img(
             :style="{'background-image': `url(${require('@/assets/curso/tema6/1.jpg')})`}") 
+          .bloque-texto-g__texto.px-5.py-4
+            p Sostener los vínculos interpersonales contribuye a la generación de habilidades individuales y sociales con la comunidad. Esto se refiere a aprender a cooperar, dialogar, expresar emociones y sentimientos, trabajar en equipo, respetar instrucciones, normas, reconocer la autoridad, cumplir con nuestras labores, negociar, empatizar y escuchar al otro.
 
-separador
 
-    #t_6_2.titulo-segundo.color-acento-contenido(data-aos="fade-right")
-      h2 6.2 Factores
+    .row.justify-content-center.mb-4.mb-lg-5
+      .col-xl-10
+        .d-flex.flex-column.flex-lg-row.justify-content-center.align-items-center.z-2.bg-secundario-claro
+          .col-6.col-lg-3
+            figure
+              img.img-205(data-aos="fade-down-right", src="@/assets/curso/tema6/2.svg", alt="descripcion")
+          .col-lg-9
+            .p-4
+              h5 Comunicación
+              p.mb-0 La clave es la comunicación. Esto implica la capacidad de transferir mensajes e información a los demás sin importar si el mensaje es positivo o negativo, si se buscan vínculos sanos se debe procurar comunicar de forma clara, sin causar heridas a nadie, comprendiendo y retroalimentando al otro.
 
-    p Según los portales <em>web</em> especializados Impulsa y Vida Profesional, la mejor forma para llegar al cliente es a través de la atención. Al momento de ofrecer productos hay que hablar de las ofertas y los descuentos, se debe estar atento a brindar una buena atención, estas acciones pueden llegar a reducir costos y benefician tanto a la empresa como al cliente.
-    p.mb-5 Los factores que intervienen en la atención al cliente son:
+    .row.justify-content-center
+      .col-xl-10
+        h4(data-aos='zoom-in-right').mb-4.mb-lg-5
+          span.etiqueta-title Relaciones interpersonales con personas en condición de discapacidad
 
-    .bg_grad-06(data-aos="zoom-in")
-      .row.justify-center
-        .col-12.col-lg-8
-          PasosA.color-acento-contenido.mb-5(tipo="n")
-            .row
-              .col-md-8.mb-4.mb-md-0.align-items-center
-                div  
-                  h3 Sociabilidad
-                  p Capacidad de interrelacionarse con otros, saber entablar conversaciones amigables, demostrar interés por las personas.
-              .col-md-4
+    .row.justify-content-center.align-items-center.mb-4.mb-lg-5.z-2
+      .col-md-8.col-lg-3
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-down-right", src="@/assets/curso/tema6/3.png", alt="imagen de acompañamiento")
+      .col-lg-7
+        p Las empresas deben preocuparse por cumplir con las expectativas que tienen sus clientes y tener en cuenta que algunas de las personas que van a atender tienen alguna discapacidad física, mental o sensorial. Las personas discapacitadas tienen los mismos derechos que el resto de la población, por lo que las empresas deben contar con un protocolo de atención al cliente en condición de discapacidad. Es decir, planear su forma de actuar frente a las circunstancias con el fin de cumplir con las políticas de inclusión que amparan a esta población. Es por esto que se hace necesario que las personas de atención al cliente estén preparadas y capacitadas para tener una actitud de servicio que supere las expectativas de los discapacitados.
+        p.mb-0 Las relaciones interpersonales que se deben dar con las personas en condición de discapacidad tienen que contar con un trato digno, respetuoso, utilizando un lenguaje adecuado acorde con la discapacidad del cliente, evitando términos excluyentes.
+
+    .row.justify-content-center.mb-4.mb-lg-5
+      .col-xl-10
+        .d-flex.flex-column.flex-lg-row.justify-content-center.align-items-center.z-2.bg-secundario-claro
+          .col-lg-9
+            .p-4
+              p.mb-0 Las personas en condición de discapacidad que ingresen a cualquier establecimiento comercial o de atención al cliente deben ser atendidas con prioridad para evitarles largos tiempos de espera, también se debe tener en cuenta permitir el ingreso de perros guía y acompañantes si los tienen, la infraestructura debe contar con espacios para la movilidad de esta población, en muchas ocasiones el funcionario debe desplazarse hasta donde se encuentre la persona en condición de discapacidad. Todos los funcionarios deben cumplir con los protocolos establecidos para atender a los clientes en condición de discapacidad.
+          .col-6.col-lg-3
+            figure
+              img.img-225(data-aos="fade-down-right", src="@/assets/curso/tema6/4.svg", alt="descripcion")
+
+    .row.justify-content-center
+      .col-xl-10
+        .cajon.color-terciario.p-4(data-aos="fade-up")
+          p.mb-0 Las empresas deben contar con señalización visual para las personas en condición de discapacidad auditiva que les permitan saber que cuenta con atención especial, en el caso de las personas en condición de discapacidad motriz deben existir rampas de ingreso y salida, para las personas con discapacidad visual debe haber lenguaje braille ubicado en sitios estratégicos para que el cliente sepa dónde dirigirse.
+
+    Separador
+
+    .row.justify-content-center
+      .col-xl-10
+        #t_6_1.titulo-segundo.color-acento-contenido(data-aos="fade-right")
+          h2 6.1 Tipos
+
+    .row.justify-content-center.align-items-center.mb-4.mb-lg-5.z-2
+      .col-10.col-md-8.col-lg-6.col-xl-3
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-down-right", src="@/assets/curso/tema6/5.png", alt="imagen de acompañamiento")
+      .col-lg-6.col-xl-7
+        p.mb-0 Las relaciones interpersonales con los clientes consisten en ofrecer una buena experiencia que radica en tratarlos con amabilidad, respeto y agilidad a través de los diferentes canales de comunicación con los que cuente la empresa. Un gran porcentaje de empresas declara que el #[em software de Customer Relationship Management] (CRM) aumentó el acceso y administración de datos de los clientes. Son las que se dan entre los individuos, empresas e instituciones y el mercado de consumo que tiene el sector comercial, se da por la necesidad de adquirir productos o servicios.
+
+    .row.justify-content-center.mb-4.mb-lg-5
+      .col-xl-10
+        p.mb-0 Ahora se señalan las relaciones interpersonales con los clientes.
+
+    .row.justify-content-center.custom-tabs
+      .col-xl-10
+        TabsA.color-primario.mb-5(data-aos="fade-down")
+          .tarjeta.bg-primario-claro.h-100.d-flex.align-items-center.p-4(titulo="Relación de asistencia personal")
+            .row.align-items-center.p-4
+              .col-12.col-lg-3.align-items-center.mb-3.mb-lg-0
                 figure
-                  img(src='@/assets/curso/tema6/t6-6.svg', alt='Texto que describa la imagen')
-            .row
-              .col-md-4.mb-3
+                  img.mb-4.mb-lg-0.img-140(src='@/assets/curso/tema6/6.svg', alt='Texto que describa la imagen')
+              .col-12.col-lg-9
+                p.mb-0 Es la que se da entre un funcionario de atención al cliente y el cliente, se puede dar de diferentes maneras tales como física (cara a cara), o a distancia (correo electrónico, telefónicamente, etc.).
+          .tarjeta.bg-primario-claro.h-100.d-flex.align-items-center.p-4(titulo="Relaciones de asistencia personal exclusiva")
+            .row.align-items-center.p-4
+              .col-12.col-lg-3.align-items-center.mb-3.mb-lg-0
                 figure
-                  img(src='@/assets/curso/tema6/t6-7.svg', alt='Texto que describa la imagen')
-              .col-md-8.mb-4.mb-md-0.align-items-center
-                div  
-                  h3 Comunicación clara y directa
-                  p La comunicación debe ser sana, se deben tener en cuenta las fortalezas de la empresa y los beneficios que se puedan ofrecer para cada caso.
-            .row
-              .col-md-8.mb-4.mb-md-0.align-items-center
-                div  
-                  h3 Honestidad
-                  p No se debe engañar a los clientes, debe darle lo que le promete, el cliente engañado es cliente perdido.
-              .col-md-4
+                  img.mb-4.mb-lg-0.img-140(src='@/assets/curso/tema6/7.svg', alt='Texto que describa la imagen')
+              .col-12.col-lg-9    
+                p.mb-0 Este tipo de asistencia se da con clientes especiales o con marcas de lujo, en estos casos asignan un representante de la empresa para brindar un acompañamiento constante al cliente en todo su proceso de compra o de uso de un servicio. Estos servicios los reciben empresarios que tienen cuentas en bancos privados, políticos importantes y celebridades.
+          .tarjeta.bg-primario-claro.h-100.d-flex.align-items-center.p-4(titulo="Relación de autoservicio")
+            .row.align-items-center.p-4
+              .col-12.col-lg-3.align-items-center.mb-3.mb-lg-0
                 figure
-                  img(src='@/assets/curso/tema6/t6-8.svg', alt='Texto que describa la imagen')
-            .row
-              .col-md-4.mb-3
+                  img.mb-4.mb-lg-0.img-140(src='@/assets/curso/tema6/8.svg', alt='Texto que describa la imagen')
+              .col-12.col-lg-9    
+                p.mb-0 En ese tipo de relación la empresa no mantiene relación directa con el cliente, pero la empresa sí ofrece las herramientas necesarias para que el cliente busque ayuda en el momento que lo requiera, estos centros de ayuda son los chatbots y foros, también Amazon y MercadoLibre que ofrecen una buena atención al cliente y le resuelven sus problemas sin necesitar un representante de servicio al cliente.
+          .tarjeta.bg-primario-claro.h-100.d-flex.align-items-center.p-4(titulo="Relaciones a través de servicios automatizados")
+            .row.align-items-center.p-4
+              .col-12.col-lg-3.align-items-center.mb-3.mb-lg-0
                 figure
-                  img(src='@/assets/curso/tema6/t6-9.svg', alt='Texto que describa la imagen')
-              .col-md-8.mb-4.mb-md-0.align-items-center
-                div  
-                  h3 Conocimiento del cliente
-                  p Conozca muy bien a su cliente, manténgase informado sobre sus necesidades y expectativas, tome la delantera frente a lo que necesita, sea proactivo, esto puede darle ventaja con respecto a la competencia que ofrece los mismos productos y servicios.
-            .row
-              .col-md-8.mb-4.mb-md-0.align-items-center
-                div  
-                  h3 Saber escuchar
-                  p Es muy importante estar atento, los clientes son el termómetro que mide nuestra productividad, esté pendiente de escuchar sus quejas y sugerencias.
-              .col-md-4
+                  img.mb-4.mb-lg-0.img-140(src='@/assets/curso/tema6/9.svg', alt='Texto que describa la imagen')
+              .col-12.col-lg-9    
+                p.mb-0 Esta relación se refiere a la inteligencia artificial (expresada por máquinas, procesadores y software), lastimosamente no es una tecnología muy usada en Colombia para establecer buenas relaciones con los clientes.
+          .tarjeta.bg-primario-claro.h-100.d-flex.align-items-center.p-4(titulo="Relaciones de comunidades")
+            .row.align-items-center.p-4
+              .col-12.col-lg-3.align-items-center.mb-3.mb-lg-0
                 figure
-                  img(src='@/assets/curso/tema6/t6-10.svg', alt='Texto que describa la imagen')
-            .row
-              .col-md-4.mb-3
-                figure
-                  img(src='@/assets/curso/tema6/t6-11.svg', alt='Texto que describa la imagen')
-              .col-md-8.mb-4.mb-md-0.align-items-center
-                div  
-                  h3 Sea creativo y efectivo
-                  p Sea innovador frente a las soluciones que le ofrezca de una manera efectiva, hágale sentir que sus necesidades son su prioridad.
-            .row
-              .col-md-8.mb-4.mb-md-0.align-items-center
-                div  
-                  h3 Busca las respuestas
-                  p No solamente debemos escuchar, algunas veces tendremos que ir a buscar las respuestas que necesitamos del cliente para mejorar el servicio, en ocasiones el cliente desconoce lo que quiere, esa es la oportunidad que tiene para ofrecerle soluciones y brindarle sus productos y servicios
-              .col-md-4
-                figure
-                  img(src='@/assets/curso/tema6/t6-12.svg', alt='Texto que describa la imagen')
+                  img.mb-4.mb-lg-0.img-140(src='@/assets/curso/tema6/10.svg', alt='Texto que describa la imagen')
+              .col-12.col-lg-9    
+                p.mb-0 Con esta relación las empresas pueden demostrar su ventaja competitiva en el mercado al integrar al cliente a través de las diferentes tecnologías, creando grupos con intereses y necesidades en común, este tipo de comunidades permite conocer de primera mano la opinión del cliente.
+
+    Separador
+
+    .row.justify-content-center
+      .col-xl-10
+        #t_6_2.titulo-segundo.color-acento-contenido(data-aos="fade-right")
+          h2 6.2 Factores
+
+    .row.justify-content-center.mb-4.mb-lg-5
+      .col-xl-10
+        p Según los portales <em>web</em> especializados Impulsa y Vida Profesional, la mejor forma para llegar al cliente es a través de la atención. Al momento de ofrecer productos hay que hablar de las ofertas y los descuentos, se debe estar atento a brindar una buena atención, estas acciones pueden llegar a reducir costos y benefician tanto a la empresa como al cliente.
+        p.mb-5 Los factores que intervienen en la atención al cliente son:
+
+    .row.justify-content-center
+      .col-12.col-lg-8
+        PasosA.color-primario.mb-5(tipo="n")(data-aos="zoom-in")
+          .row
+            .col-md-8.mb-4.mb-md-0.align-items-center
+              div  
+                h3 Sociabilidad
+                p Capacidad de interrelacionarse con otros, saber entablar conversaciones amigables, demostrar interés por las personas.
+            .col-md-4
+              figure
+                img(src='@/assets/curso/tema6/11.svg', alt='Texto que describa la imagen')
+          .row
+            .col-md-4.mb-3
+              figure
+                img(src='@/assets/curso/tema6/12.svg', alt='Texto que describa la imagen')
+            .col-md-8.mb-4.mb-md-0.align-items-center
+              div  
+                h3.text-start Comunicación clara y directa
+                p.text-start La comunicación debe ser sana, se deben tener en cuenta las fortalezas de la empresa y los beneficios que se puedan ofrecer para cada caso.
+          .row
+            .col-md-8.mb-4.mb-md-0.align-items-center
+              div  
+                h3 Honestidad
+                p No se debe engañar a los clientes, debe darle lo que le promete, el cliente engañado es cliente perdido.
+            .col-md-4
+              figure
+                img(src='@/assets/curso/tema6/13.svg', alt='Texto que describa la imagen')
+          .row
+            .col-md-4.mb-3
+              figure
+                img(src='@/assets/curso/tema6/14.svg', alt='Texto que describa la imagen')
+            .col-md-8.mb-4.mb-md-0.align-items-center
+              div  
+                h3.text-start Conocimiento del cliente
+                p.text-start Conozca muy bien a su cliente, manténgase informado sobre sus necesidades y expectativas, tome la delantera frente a lo que necesita, sea proactivo, esto puede darle ventaja con respecto a la competencia que ofrece los mismos productos y servicios.
+          .row
+            .col-md-8.mb-4.mb-md-0.align-items-center
+              div  
+                h3 Saber escuchar
+                p Es muy importante estar atento, los clientes son el termómetro que mide nuestra productividad, esté pendiente de escuchar sus quejas y sugerencias.
+            .col-md-4
+              figure
+                img(src='@/assets/curso/tema6/15.svg', alt='Texto que describa la imagen')
+          .row
+            .col-md-4.mb-3
+              figure
+                img(src='@/assets/curso/tema6/16.svg', alt='Texto que describa la imagen')
+            .col-md-8.mb-4.mb-md-0.align-items-center
+              div  
+                h3.text-start Sea creativo y efectivo
+                p.text-start Sea innovador frente a las soluciones que le ofrezca de una manera efectiva, hágale sentir que sus necesidades son su prioridad.
+          .row
+            .col-md-8.mb-4.mb-md-0.align-items-center
+              div  
+                h3.text-start Busca las respuestas
+                p.text-start No solamente debemos escuchar, algunas veces tendremos que ir a buscar las respuestas que necesitamos del cliente para mejorar el servicio, en ocasiones el cliente desconoce lo que quiere, esa es la oportunidad que tiene para ofrecerle soluciones y brindarle sus productos y servicios
+            .col-md-4
+              figure
+                img(src='@/assets/curso/tema6/17.svg', alt='Texto que describa la imagen')
 </template>
 
 <script>
